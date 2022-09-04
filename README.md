d1abbc3c5f9b5c752e2706f418e50e747d43e5cb
hwollin <2457935544@qq.com>
2022-09-05 07:05:05 +0800

    add epoll

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
 # hwx
 在zero分支的基础上，添加epoll
 
-### 接口文档
+### epoll简介
+
++ monitoring multiple file descriptors to see if I/O is possible on any of them.
++ The epoll API can be used either as an edge-triggered or a level-triggered
+    + level-t(default): 只要监听的文件描述符中有数据，就会触发epoll_wait返回
+    + edge-t:  只有监听的文件描述符的读/写事件发生，才会触发epoll_wait返回
++ epoll instance is an in-kernel data structure which has two lists:
+    + interest list: all fds
+    + ready list: fds that are ready for I/O
+
+### 相关文档
 #### epoll
 + [epoll man page](https://man7.org/linux/man-pages/man7/epoll.7.html)
+    + epoll_create(): create epoll instance
+    + epoll_ctl(): add/del/... fd to epoll
+    + epoll_wait(): wait for I/O events, blocking if no events are available
++ [水平触发和边沿触发](https://zhuanlan.zhihu.com/p/107995399)
++ epoll events:
+  + [EPOLLIN和EPOLLOUT的触发条件](https://blog.csdn.net/zgaoq/article/details/103807337)
--- src/hwx.c
+++ src/hwx.c
@@ -4,14 +4,17 @@
 
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <sys/epoll.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <string.h>
+#include <fcntl.h>
 #include <stdio.h>
 
-#define CONN_QUEUE_SIZE 10
+#define CONN_QUEUE_SIZE 1024
 #define BUFF_SIZE 1024
 #define INDEX_HTML "../html/index.html"
+#define MAX_EVENTS 1024
 
 //在http1.1及之后版本。如果是keep alive，则content-length和chunk必然是二选一
 //否则当你用浏览器访问时，响应体会无法显示
@@ -19,73 +22,94 @@
 
 void write_response_to_buff(const char *file, char *buff);
 
+void setnonblocking(int sock);
+
 int main(int argc, char *const *argv) {
-    // <1> server socket
     int listen_sock = socket(AF_INET, SOCK_STREAM, 0);
 
+    setnonblocking(listen_sock);
 
-    // <2> server socket address info
     struct sockaddr_in servaddr;
-
     memset(&servaddr, 0, sizeof(servaddr));
 
-    // IPv4 Internet protocols
     servaddr.sin_family = AF_INET;
-
-    // converts the unsigned short integer 80 from host byte order to network byte order.
     servaddr.sin_port = htons(8088);
-
-    // Address to accept any incoming messages.
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-
-    // <2> -> <1> : assign address "servaddr" to socket "listen_sock"
     bind(listen_sock, (struct sockaddr *) &servaddr, sizeof(servaddr));
 
-    // <3> listen for connections on "listen_sock", 10 connection requests will be queued before
-    // further requests are refused.
     listen(listen_sock, CONN_QUEUE_SIZE);
 
-    // <4.1> client address info
+    // client address info
     struct sockaddr_in cliaddr;
     socklen_t cliaddr_len;
 
-    // <6.1> buffer to read request
+    // buffer to read request
     char req_buff[BUFF_SIZE] = {'\0'};
     // 单次recv()接收到的字节数
     int recv_len = 0;
 
-    // <7.1> buffer to send response
+    // buffer to send response
     char resp_buff[BUFF_SIZE] = {'\0'};
     write_response_to_buff(INDEX_HTML, resp_buff);
 
+    // epoll
+    int epollfd = epoll_create1(0);
+    struct epoll_event ev, events[MAX_EVENTS];
+    ev.events = EPOLLIN;
+    ev.data.fd = listen_sock;
+
+    // number of fd that events happen
+    int nfds;
+
+    // add listen_sock to epoll
+    epoll_ctl(epollfd, EPOLL_CTL_ADD, listen_sock, &ev);
+
     while (1) {
-        // <4> Await a connection on socket "listen_sock". When a connection arrives, open a new socket
-        // to communicate with it, and return the new socket's fd
-        int conn_sock = accept(listen_sock, (struct sockaddr *) &cliaddr, &cliaddr_len);
-        //printf("connection----------->%d\n", conn_sock);
+        //阻塞直到有事件发生
+        nfds = epoll_wait(epollfd, events, MAX_EVENTS, -1);
 
-        // <5> print client address info
-        // inet_ntoa : converts the Internet host address "cliaddr.sin_addr" to "string ip address"
-        printf("connected from %s\n", inet_ntoa(cliaddr.sin_addr));
+        //遍历发生的事件
+        for (int i = 0; i < nfds; i++) {
+            if (events[i].data.fd == listen_sock) {  // 有新的连接到来
+                int conn_sock = accept(listen_sock, (struct sockaddr *) &cliaddr, &cliaddr_len);
+                setnonblocking(conn_sock);
 
-        // <6> read request
-        // recv() returns the number of bytes received, if no message are avaiable at the
-        // socket, the recv() func wait for a message to arrive.
-        // 如果请求的大小超过了buff的大小，那就多次读取
-        do {
-            recv_len = recv(conn_sock, req_buff, BUFF_SIZE, 0);
-            printf("%s", req_buff);
-            memset(req_buff, 0, sizeof(req_buff));
-        } while (recv_len == BUFF_SIZE);
-        printf("\n");
+                // print client ip
+                printf("hello %s\n", inet_ntoa(cliaddr.sin_addr));
 
-        // <7> send index.html to client
-        // 仅仅是把resp_buff中的数据copy到发送缓冲区，而不会真正等待发送完成
-        send(conn_sock, resp_buff, strlen(resp_buff), 0);
+                // add conn_sock to epoll
+                ev.events = EPOLLIN | EPOLLET;
+                ev.data.fd = conn_sock;
+                epoll_ctl(epollfd, EPOLL_CTL_ADD, conn_sock, &ev);
+            } else if (events[i].events & EPOLLIN) {  // conn_sock read
+                int conn_sock = events[i].data.fd;
 
-        // <8> close conn socket
-        //close(conn_sock);
+                // print request
+                do {
+                    recv_len = recv(conn_sock, req_buff, BUFF_SIZE, 0);
+                    printf("%s", req_buff);
+                    memset(req_buff, 0, sizeof(req_buff));
+                } while (recv_len == BUFF_SIZE);
+                printf("\n");
+
+                // 获取请求之后要去响应
+                ev.events = EPOLLOUT | EPOLLET;
+                ev.data.fd = conn_sock;
+                // 会触发EPOLLOUT事件
+                epoll_ctl(epollfd, EPOLL_CTL_MOD, conn_sock, &ev);
+            } else if (events[i].events & EPOLLOUT) {  // conn_sock write
+                int conn_sock = events[i].data.fd;
+                // send response
+                send(conn_sock, resp_buff, strlen(resp_buff), 0);
+
+                // 响应后要继续关注请求
+                ev.events = EPOLLIN | EPOLLET;
+                ev.data.fd = conn_sock;
+                epoll_ctl(epollfd, EPOLL_CTL_MOD, conn_sock, &ev);
+            }
+
+        }
     }
 }
 
@@ -100,4 +124,10 @@ void write_response_to_buff(const char *file, char *buff) {
     FILE *f = fopen(file, "rb");
     fread(buff + header_len, 1, BUFF_SIZE, f);
     fclose(f);
+}
+
+// 将socket(默认阻塞)设置为非阻塞
+void setnonblocking(int sock) {
+    int opts = fcntl(sock, F_GETFL);
+    fcntl(sock, F_SETFL, opts | O_NONBLOCK);
 }
